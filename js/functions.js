551bc4bb76d111a94a6ebb4b2635c6552568e3df
MimiMc4 <miguelmarclave@gmail.com>
2025-08-18 19:36:26 +0200

    brightness article

--- js/functions.js
+++ js/functions.js
@@ -14,6 +14,8 @@ function scrollFunction() {
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  // For Safari
+  document.body.scrollTop = 0; 
+  // For Chrome, Firefox, IE and Opera
+  document.documentElement.scrollTop = 0; 
 }
